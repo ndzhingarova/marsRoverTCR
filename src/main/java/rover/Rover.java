be51f7503da9ffaad0c9dc75247bfde9be45ba44
Nikoleta Dzhingarova <n.dzhingarova@gmail.com>
2021-08-29 12:57:44 -0400

    working

--- src/main/java/rover/Rover.java
+++ src/main/java/rover/Rover.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Rover {
-    private static List<String> compass = Arrays.asList("N", "E", "S", "W");
 
     private String direction;
 
@@ -21,7 +20,7 @@ public class Rover {
         if ("L".equals(command)) {
             this.direction = "W";
         } else {
-            turn(compass);
+            turn(Arrays.asList("N", "E", "S", "W"));
         }
     }
 
